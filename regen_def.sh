6e1a2d9ef6e6dad06bf3e3a5ed73613becd2dfa1
dndxtz <dindascape@gmail.com>
2023-09-24 17:15:16 +0700

    script: update

--- regen_def.sh
+++ regen_def.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DEFCONFIG="rolex_defconfig"
+DEFCONFIG="S88503AC1-perf_defconfig"
 
 make -j"$(nproc --all)" O=out ARCH=arm64 SUBARCH=arm64 "$DEFCONFIG"
 cp -af out/.config arch/arm64/configs/"$DEFCONFIG"
