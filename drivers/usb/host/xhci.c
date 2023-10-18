b9b8eff031f5cdd6c0f1f89651c36cfb4b4d8c68
dndxtz <dindascape@gmail.com>
2023-10-18 10:36:31 +0700

    Merge tag 'v3.18.33' into memehui
    Linux 3.18.33

--- drivers/usb/host/xhci.c
+++ drivers/usb/host/xhci.c
@@ -3840,8 +3840,6 @@ static int xhci_setup_device(struct usb_hcd *hcd, struct usb_device *udev,
 	u64 temp_64;
 	struct xhci_command *command = NULL;
 
-	mutex_lock(&xhci->mutex);
-
 	if (xhci->xhc_state)	/* dying, removing or halted */
 		return -EINVAL;
 
